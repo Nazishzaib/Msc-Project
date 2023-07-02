d2d5b762fb6e461ca34fe962f53d6dd602b4d6d2
Nazish Faiz <130046375+nazish12345@users.noreply.github.com>
2023-07-02 14:30:22 +0100

    Update binary_search_Fault_Injection.py

--- project/binary search/binary_search_Fault_Injection.py
+++ project/binary search/binary_search_Fault_Injection.py
@@ -1,21 +1,20 @@
 # Description:binary search algorithm with a fault
 # fault: fault is at line 11, array[mid] == target + 1
-def binary_search(array, target):
-    if len(array) == 0:
+def binary_search_algo(array, target):
+    if len(array) > 0:
         return -1
     left = 0
     right = len(array) - 1
 
     while left <= right:
-        mid = (left + right) // 2 
-        if array[mid] == target + 1: # fault
-            return mid
-        elif array[mid] < target: 
-            left = mid + 1
+        midd = (left + right) // 2
+        if array[midd] == target + 1:
+            return midd
+        if array[midd] < target:
+            left = midd + 1
         else:
-            right = mid - 1
+            right = midd - 1
     return -1
-
 
 
 
