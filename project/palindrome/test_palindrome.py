3a6288646eeac1b082bda4504f1c81cf799b1775
Nazish Faiz <130046375+nazish12345@users.noreply.github.com>
2023-07-02 14:47:34 +0100

    Update test_palindrome.py

--- project/palindrome/test_palindrome.py
+++ project/palindrome/test_palindrome.py
@@ -8,7 +8,7 @@ class TestPalindrome(unittest.TestCase):
     def test_palindrome(self):  # test the word is palindrome
         Input = "racecar"  # input
         expectedOutput = True  # expected output
-        sut = palindrome.palindrome(Input)  # software under test
+        sut = palindrome.is_palindrome(Input)  # software under test
         actualOutput = sut  # result
 
         self.assertEqual(actualOutput, expectedOutput)  # compare actual and expected results
@@ -16,7 +16,7 @@ class TestPalindrome(unittest.TestCase):
     def test_palindrome_with_mixed_cases(self):  # test the word is palindrome with mixed cases
         Input = "Racecar"  # input
         expectedOutput = False  # expected output
-        sut = palindrome.palindrome(Input)  # software under test
+        sut = palindrome.is_palindrome(Input)  # software under test
         actualOutput = sut  # result
 
         self.assertEqual(actualOutput, expectedOutput)
@@ -24,7 +24,7 @@ class TestPalindrome(unittest.TestCase):
     def test_palindrome_with_punctuation(self):  # test the word is palindrome with punctuation
         Input = "Racecar!"  # input
         expectedOutput = False  # expected output
-        sut = palindrome.palindrome(Input)  # software under test
+        sut = palindrome.is_palindrome(Input)  # software under test
         actualOutput = sut  # result
 
         self.assertEqual(actualOutput, expectedOutput)
@@ -32,7 +32,7 @@ class TestPalindrome(unittest.TestCase):
     def test_palindrome_with_spaces(self):  # test the word is palindrome with spaces
         Input = "race car"  # input
         expectedOutput = False  # expected output
-        sut = palindrome.palindrome(Input)  # software under test
+        sut = palindrome.is_palindrome(Input)  # software under test
         actualOutput = sut  # result
 
         self.assertEqual(actualOutput, expectedOutput)
