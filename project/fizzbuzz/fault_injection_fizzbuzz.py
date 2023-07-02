dfed93911e1ad59ef1d25535531d5b57b4fcf5e8
Nazish Faiz <130046375+nazish12345@users.noreply.github.com>
2023-07-02 14:37:40 +0100

    Update fault_injection_fizzbuzz.py

--- project/fizzbuzz/fault_injection_fizzbuzz.py
+++ project/fizzbuzz/fault_injection_fizzbuzz.py
@@ -1,9 +1,11 @@
-def fizz_buzz(i):
-    if i % 15 == 0:
+# Description: Fault injection for FizzBuzz
+# fault: fault is at line 6
+def fiz_buz_func(r):
+    if r % 15 == 0:
         return 'FizzBuzz'
-    elif i % 3 == 1:
+    if r % 3 == 1:  # fault
         return 'Fizz'
-    elif i % 5 == 0:
+    if r % 5 == 0:
         return 'Buzz'
     else:
-        return str(i)
+        return r
